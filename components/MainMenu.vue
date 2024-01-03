2eaa0df6025da56d2e8611125662e3133e458142
Gustavo Barragan <dev@droni.co>
2024-01-02 21:55:09 -0500

    main menu

---
+++ components/MainMenu.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="fixed top-0 left-0 z-40 h-screen p-4 overflow-y-auto transition-transform bg-white w-80 dark:bg-gray-800" :class="{ '-translate-x-full': menu }">
+    <h5 id="drawer-label" class="inline-flex items-center mb-4 text-base font-semibold text-gray-500 dark:text-gray-400">Info</h5>
+    <button
+      type="button"
+      data-drawer-hide="drawer-example"
+      aria-controls="drawer-example"
+      class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 absolute top-2.5 end-2.5 flex items-center justify-center dark:hover:bg-gray-600 dark:hover:text-white"
+      @click="menu = true">
+        x
+        <span class="sr-only">Close menu</span>
+    </button>
+        
+    <p class="mb-6 text-sm text-gray-500 dark:text-gray-400">Supercharge your hiring by taking advantage of our <a href="#" class="text-blue-600 underline dark:text-blue-500 hover:no-underline">limited-time sale</a> for Flowbite Docs + Job Board. Unlimited access to over 190K top-ranked candidates and the #1 design job board.</p>
+    <div class="grid grid-cols-2 gap-4">
+        <a href="#" class="px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-200 rounded-lg focus:outline-none hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Learn more</a>
+        <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Get access <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+    </svg></a>
+    </div>
+  </div>
+  <button class="text-4xl text-white" @click="menu = false">
+    <i class="mdi mdi-menu"></i>
+  </button>
+</template>
+<script setup lang="ts">
+const menu = ref(true)
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,16 @@
 <template>
+  <!-- drawer component -->
   <div class="min-h-screen radialBg">
     <div class="bg-gradient-to-b from-slate-800 to-transparent">
       <header class="flex py-3 container mx-auto">
-        <button class="text-4xl text-white">
-          <i class="mdi mdi-menu"></i>
-        </button>
+        <MainMenu />
         <div class="grow text-center">
           <NuxtLink to="/" title="Droni.co Movies" class="inline-block">
             <img src="~/assets/img/logo-w.svg" alt="Droni.co Movies" class="w-10" />
           </NuxtLink>
         </div>
         <button class="text-4xl text-white">
-          <i class="mdi mdi-menu"></i>
+          <i class="mdi mdi-account"></i>
         </button>
       </header>
     </div>
@@ -31,6 +30,9 @@
     </footer>
   </div>
 </template>
+<script setup lang="ts">
+
+</script>
 <style scoped>
   .radialBg {
     background: rgb(71,85,105);
