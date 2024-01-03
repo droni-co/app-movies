9cae2ad19ecc82eff25fd480a283ff2ce1fee9e3
Gustavo Barragan <dev@droni.co>
2024-01-02 21:41:18 -0500

    new layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,39 @@
 <template>
-  <div class="min-h-screen">
-    <header class="flex container mx-auto">
-      <NuxtLink to="/" class="bg-sky-800 text-white p-3 transition hover:bg-sky-700">
-        Movies
-      </NuxtLink>
-      <nav class="grow text-end">
-        <NuxtLink to="/search" class="inline-block rounded bg-sky-800 text-white py-1 px-3 my-2 transition hover:bg-sky-700">
-          Search
-        </NuxtLink>
-      </nav>
-    </header>
+  <div class="min-h-screen radialBg">
+    <div class="bg-gradient-to-b from-slate-800 to-transparent">
+      <header class="flex py-3 container mx-auto">
+        <button class="text-4xl text-white">
+          <i class="mdi mdi-menu"></i>
+        </button>
+        <div class="grow text-center">
+          <NuxtLink to="/" title="Droni.co Movies" class="inline-block">
+            <img src="~/assets/img/logo-w.svg" alt="Droni.co Movies" class="w-10" />
+          </NuxtLink>
+        </div>
+        <button class="text-4xl text-white">
+          <i class="mdi mdi-menu"></i>
+        </button>
+      </header>
+    </div>
     <main class="container mx-auto">
       <slot />
     </main>
+    <footer class="bg-gradient-to-b from-slate-800 to-slate-900 py-5">
+      <div class="text-white text-center py-2">
+        <p class="text-sm">
+          ©2024 Colombia.<br>
+          Construido con trabajo duro y mucho café por el equipo de 
+          <a href="https://droni.co" target="_blank" rel="noopener" class="underline">
+            Droni.co.
+          </a>
+        </p>
+      </div>
+    </footer>
   </div>
 </template>
+<style scoped>
+  .radialBg {
+    background: rgb(71,85,105);
+    background: radial-gradient(circle, rgba(71,85,105,1) 0%, rgba(30,41,59,1) 100%);
+  }
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,18 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
     '@nuxtjs/tailwindcss'
-  ]
+  ],
+  app: {
+    head: {
+      title: 'Droni.co Movies',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: "stylesheet", href: "https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/7.4.47/css/materialdesignicons.min.css"}
+      ]
+    }
+  }
 })
