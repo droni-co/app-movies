f1ad1aff70de638b72f716efbba0f163946da6b9
Gustavo Barragan <dev@droni.co>
2024-01-02 21:08:17 -0500

    Init

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="min-h-screen">
+    <header class="flex container mx-auto">
+      <NuxtLink to="/" class="bg-sky-800 text-white p-3 transition hover:bg-sky-700">
+        Movies
+      </NuxtLink>
+      <nav class="grow text-end">
+        <NuxtLink to="/search" class="inline-block rounded bg-sky-800 text-white py-1 px-3 my-2 transition hover:bg-sky-700">
+          Search
+        </NuxtLink>
+      </nav>
+    </header>
+    <main class="container mx-auto">
+      <slot />
+    </main>
+  </div>
+</template>
---
+++ pages/search.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>Search page</h1>
+    <!-- Esto es un comentario -->
+  </div>
+</template>
+
+// esto es un comentario
